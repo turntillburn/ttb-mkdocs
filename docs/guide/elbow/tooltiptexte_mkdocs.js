e5f0b8f7652ec18651e3d4235e0a972330c688f0
Alexander Schneider <alexander.schneider@uzh.ch>
2026-02-19 21:25:59 +0100

    Refine elbow tooltips and unify navigation behaviour
    - Remove popup windows (open detail pages in same tab)
    - Fix close button to use history.back()
    - Improve mobile tooltip readability
    - Minor tooltip visibility adjustments

--- docs/guide/elbow/tooltiptexte_mkdocs.js
+++ docs/guide/elbow/tooltiptexte_mkdocs.js
@@ -1,124 +1,116 @@
-// MkDocs-Port – Tooltiptexte Ellbogen
-// Öffnet Detailseiten aus ./tooti_elbow/ in einem Popup (wie früher)
-
-function ttbOpen(url, winName) {
-  const win = window.open(url, winName, "scrollbars=1,width=310,height=400,left=400,top=100");
-  if (win) win.focus();
-  return false;
+function ttbLink(label, file) {
+  return `<a href="./tooti_elbow/${file}">${label}</a>`;
 }
 
-function ttbLink(label, file, winName) {
-  return `<a href="#" onclick="return ttbOpen('./tooti_elbow/${file}','${winName}')">${label}</a>`;
-}
 
 var el_01 =
-  ttbLink("Golferellbogen", "Golferellbogen.htm", "Fenster1") + "<br>" +
-  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm", "Fenster1") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm", "Fenster1") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm", "Fenster3") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Golferellbogen", "Golferellbogen.htm") + "<br>" +
+  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm") + "<br>" +
+  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_02 =
-  ttbLink("Schleimbeutelentz&uuml;ndung", "Schleimbeutelentzuendung.htm", "Fenster1") + "<br>" +
-  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm", "Fenster1") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm", "Fenster3") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Schleimbeutelentz&uuml;ndung", "Schleimbeutelentzuendung.htm") + "<br>" +
+  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm") + "<br>" +
+  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_03 =
-  ttbLink("Bizepssehnenentz&uuml;ndung", "Bizepssehnenentzuendung.htm", "Fenster1") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm", "Fenster1") + "<br>" +
-  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm", "Fenster2") + "<br>" +
-  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm", "Fenster3") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Bizepssehnenentz&uuml;ndung", "Bizepssehnenentzuendung.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm") + "<br>" +
+  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm") + "<br>" +
+  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_04 =
-  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4") + "<br>" +
-  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm", "Fenster1");
+  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm") + "<br>" +
+  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm");
 
 var el_05 =
-  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Bizepssehnenentz&uuml;ndung", "Bizepssehnenentzuendung.htm", "Fenster1") + "<br>" +
-  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm", "Fenster1") + "<br>" +
-  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm", "Fenster3") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4") + "<br>" +
-  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm", "Fenster3") + "<br>" +
-  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm", "Fenster4") + "<br>" +
-  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm", "Fenster2");
+  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Bizepssehnenentz&uuml;ndung", "Bizepssehnenentzuendung.htm") + "<br>" +
+  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm") + "<br>" +
+  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm") + "<br>" +
+  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm") + "<br>" +
+  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm") + "<br>" +
+  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm");
 
 var el_06 =
-  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm", "Fenster1") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm", "Fenster2") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4") + "<br>" +
-  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm", "Fenster3") + "<br>" +
-  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm", "Fenster4");
+  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm") + "<br>" +
+  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm") + "<br>" +
+  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm") + "<br>" +
+  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm");
 
 var el_07 =
-  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm", "Fenster1") + "<br>" +
-  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm", "Fenster2") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4") + "<br>" +
-  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm", "Fenster3") + "<br>" +
-  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm", "Fenster4");
+  ttbLink("Drucksch&auml;digung des Medianus Nerven", "Druckschaedigung_Medianus_Nerven.htm") + "<br>" +
+  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm") + "<br>" +
+  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm") + "<br>" +
+  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm");
 
 var el_08 =
-  ttbLink("Bizepssehnenentz&uuml;ndung", "Bizepssehnenentzuendung.htm", "Fenster1") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Bizepssehnenentz&uuml;ndung", "Bizepssehnenentzuendung.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_09 =
-  ttbLink("Tennisellbogen", "Tennisellbogen.htm", "Fenster2") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm", "Fenster3") + "<br>" +
-  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm", "Fenster1");
+  ttbLink("Tennisellbogen", "Tennisellbogen.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm") + "<br>" +
+  ttbLink("Bandinstabilit&auml;t", "Bandinstabilitaet.htm");
 
 var el_10 =
-  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm", "Fenster2") + "<br>" +
-  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm", "Fenster3") + "<br>" +
-  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm", "Fenster3") + "<br>" +
-  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm", "Fenster4") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm") + "<br>" +
+  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm") + "<br>" +
+  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm") + "<br>" +
+  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_11 =
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4") + "<br>" +
-  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm", "Fenster1");
+  ttbLink("Knochenbruch", "Knochenbruch.htm") + "<br>" +
+  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm");
 
 var el_12 =
-  ttbLink("Schleimbeutelentz&uuml;ndung", "Schleimbeutelentzuendung.htm", "Fenster1") + "<br>" +
-  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm", "Fenster1") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm", "Fenster3") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Schleimbeutelentz&uuml;ndung", "Schleimbeutelentzuendung.htm") + "<br>" +
+  ttbLink("Tricepssehnenentz&uuml;ndung", "Tricepssehnenentzuendung.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Ellen Nerven", "Druckschaedigung_Ellen_Nerven.htm") + "<br>" +
+  ttbLink("Ellbogengelenk", "Ellbogengelenk.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_13 =
-  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_14 =
-  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_15 =
-  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm", "Fenster2") + "<br>" +
-  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm", "Fenster3") + "<br>" +
-  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm", "Fenster4") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm") + "<br>" +
+  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm") + "<br>" +
+  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_16 =
-  ttbLink("Sehnenscheidenentz&uuml;ndung 1. Strecksehnenfach", "Sehnenscheidenentzuendung_erstes_Strecksehnenfach.htm", "Fenster4") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Sehnenscheidenentz&uuml;ndung 1. Strecksehnenfach", "Sehnenscheidenentzuendung_erstes_Strecksehnenfach.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_17 =
-  ttbLink("Sehnenscheidenentz&uuml;ndung 2. Strecksehnenfach", "Sehnenscheidenentzuendung_zweites_Strecksehnenfach.htm", "Fenster4") + "<br>" +
-  ttbLink("Drucksch&auml;digung des oberfl&auml;chlichen Speichen Nerven", "Druckschaedigung_oberflaechlicher_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Sehnenscheidenentz&uuml;ndung 2. Strecksehnenfach", "Sehnenscheidenentzuendung_zweites_Strecksehnenfach.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des oberfl&auml;chlichen Speichen Nerven", "Druckschaedigung_oberflaechlicher_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
 
 var el_18 =
-  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm", "Fenster2") + "<br>" +
-  ttbLink("Drucksch&auml;digung des oberfl&auml;chlichen Speichen Nerven", "Druckschaedigung_oberflaechlicher_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm", "Fenster2") + "<br>" +
-  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm", "Fenster3") + "<br>" +
-  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm", "Fenster4") + "<br>" +
-  ttbLink("Knochenbruch", "Knochenbruch.htm", "Fenster4");
+  ttbLink("Unterarmmuskulatur", "Unterarmmuskulatur.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des oberfl&auml;chlichen Speichen Nerven", "Druckschaedigung_oberflaechlicher_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Drucksch&auml;digung des Speichen Nerven", "Druckschaedigung_Speichen_Nerven.htm") + "<br>" +
+  ttbLink("Akutes Logensyndrom", "Akutes_Logensyndrom.htm") + "<br>" +
+  ttbLink("Chronisches Logensyndrom", "Chronisches_Logensyndrom.htm") + "<br>" +
+  ttbLink("Knochenbruch", "Knochenbruch.htm");
--- docs/javascripts/close-fallback.js
+++ docs/javascripts/close-fallback.js
@@ -25,3 +25,41 @@
   };
 })();
 
+// ------------------------------------------------------------------
+// Always hide tooltip on interaction and on (instant) navigation
+// ------------------------------------------------------------------
+(function () {
+  const hide = () => {
+    try {
+      if (typeof window.hideTooltip === "function") window.hideTooltip();
+    } catch (_) {}
+  };
+
+  // 1) Interactions: pointerdown catches mouse + touch early
+  document.addEventListener("pointerdown", hide, true);
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") hide();
+  }, true);
+
+  // 2) Navigation changes (MkDocs Material Instant Navigation etc.)
+  // Hide when history state changes (pushState/replaceState/popstate)
+  const _pushState = history.pushState;
+  history.pushState = function () {
+    hide();
+    return _pushState.apply(this, arguments);
+  };
+
+  const _replaceState = history.replaceState;
+  history.replaceState = function () {
+    hide();
+    return _replaceState.apply(this, arguments);
+  };
+
+  window.addEventListener("popstate", hide);
+  window.addEventListener("hashchange", hide);
+
+  // 3) If Material exposes document$ (common), hide after each page swap
+  if (window.document$ && typeof window.document$.subscribe === "function") {
+    window.document$.subscribe(hide);
+  }
+})();
